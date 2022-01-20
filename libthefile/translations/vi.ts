884fdf90dd99b61871e0d3166f4221bd32168a2a
Victor Tran <vicr12345@gmail.com>
2022-01-20 23:49:48 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (68 of 68 strings)
    
    Translation: theFile/libthefile
    Translate-URL: https://translate.vicr123.com/projects/thefile/libthefile/vi/

--- libthefile/translations/vi.ts
+++ libthefile/translations/vi.ts
@@ -6,197 +6,197 @@
     <message>
         <location filename="../filecolumn.cpp" line="126"/>
         <source>Root</source>
-        <translation type="unfinished"></translation>
+        <translation>Gốc hệ thống</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="127"/>
         <location filename="../filecolumn.cpp" line="216"/>
         <location filename="../filecolumn.cpp" line="471"/>
         <source>Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>Thùng rác</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="131"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Thư mục chính</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="203"/>
         <location filename="../filecolumn.cpp" line="559"/>
         <source>New Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Thư mục mới</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="203"/>
         <source>What are you naming this folder?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn gọi thư mục này tên gì?</translation>
     </message>
     <message numerus="yes">
         <location filename="../filecolumn.cpp" line="217"/>
         <source>Moved %n items to the trash</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Đã chuyển %n mục sang thùng rác</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="253"/>
         <location filename="../filecolumn.cpp" line="357"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Đổi tên</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="253"/>
         <source>What do you want to name this item?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn muốn gọi mục này tên gì?</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="284"/>
         <location filename="../filecolumn.cpp" line="675"/>
         <source>For %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho %1</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="287"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Mở</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="295"/>
         <source>Open With...</source>
-        <translation type="unfinished"></translation>
+        <translation>Mở bằng...</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="311"/>
         <source>Another app...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ứng dụng khác...</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="319"/>
         <source>Remove from bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Xóa khỏi thẻ đánh dấu</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="323"/>
         <source>Add to bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Thêm vào thẻ đánh dấu</translation>
     </message>
     <message numerus="yes">
         <location filename="../filecolumn.cpp" line="329"/>
         <source>For %n items</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Cho %n mục</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="333"/>
         <source>Cut</source>
-        <translation type="unfinished"></translation>
+        <translation>Cắt</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="334"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Chép</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="336"/>
         <source>Restore</source>
-        <translation type="unfinished"></translation>
+        <translation>Khôi phục</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="348"/>
         <source>Delete Permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Xóa vĩnh viễn</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="350"/>
         <source>Move to Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>Chuyển sang thùng rác</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="366"/>
         <source>Properties</source>
-        <translation type="unfinished"></translation>
+        <translation>Thuộc tính</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="377"/>
         <source>Burn Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi nội dung vào đĩa</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="440"/>
         <source>No items here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Không có mục nào đây!</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="441"/>
         <source>This folder is empty.</source>
-        <translation type="unfinished"></translation>
+        <translation>Thư mục này trống.</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="444"/>
         <source>Not Found</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể tìm</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="445"/>
         <source>This folder doesn&apos;t exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>Thư mục này không tồn tại</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="448"/>
         <source>Permission Denied</source>
-        <translation type="unfinished"></translation>
+        <translation>Quyền bị từ chối</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="449"/>
         <source>Looks like you don&apos;t have permission to view this folder.</source>
-        <translation type="unfinished"></translation>
+        <translation>Có vẻ như bạn không có quyền truy cập thư mục này.</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="452"/>
         <source>Can&apos;t view this folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể xem thư mục</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="453"/>
         <source>We can&apos;t show you the contents of this folder.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúng tôi không thể hiển thị nội dung của thư mục này.</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="472"/>
         <source>Empty Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>Đổ thùng rác</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="558"/>
         <location filename="../filecolumn.cpp" line="685"/>
         <source>For this folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho thư mục này</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="560"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>Dán</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="676"/>
         <source>Copy In</source>
-        <translation type="unfinished"></translation>
+        <translation>Chép vào</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="679"/>
         <source>Move In</source>
-        <translation type="unfinished"></translation>
+        <translation>Chuyển vào</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="686"/>
         <source>Copy Here</source>
-        <translation type="unfinished"></translation>
+        <translation>Chép đây</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="689"/>
         <source>Move Here</source>
-        <translation type="unfinished"></translation>
+        <translation>Chuyển đây</translation>
     </message>
 </context>
 <context>
@@ -204,8 +204,8 @@
     <message numerus="yes">
         <location filename="../filecolumnfloater.cpp" line="51"/>
         <source>%n items</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>$n mục</numerusform>
         </translation>
     </message>
 </context>
@@ -214,146 +214,146 @@
     <message>
         <location filename="../sidebar/sidebar.ui" line="74"/>
         <source>PLACES</source>
-        <translation type="unfinished"></translation>
+        <translation>ĐỊA ĐIỂM</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.ui" line="103"/>
         <source>DRIVES</source>
-        <translation type="unfinished"></translation>
+        <translation>Ổ ĐĨA</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.ui" line="135"/>
         <source>BOOKMARKS</source>
-        <translation type="unfinished"></translation>
+        <translation>THẺ ĐÁNH DẤU</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="66"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Thư mục chính</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="67"/>
         <source>Documents</source>
-        <translation type="unfinished"></translation>
+        <translation>Tài liệu</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="68"/>
         <source>Downloads</source>
-        <translation type="unfinished"></translation>
+        <translation>Tải xuống</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="69"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhạc</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="70"/>
         <source>Pictures</source>
-        <translation type="unfinished"></translation>
+        <translation>Hình ảnh</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="71"/>
         <source>Videos</source>
-        <translation type="unfinished"></translation>
+        <translation>Ảnh động</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="72"/>
         <source>Root</source>
-        <translation type="unfinished"></translation>
+        <translation>Gốc hệ thống</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="73"/>
         <source>Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>Thùng rác</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="192"/>
         <location filename="../sidebar/sidebar.cpp" line="296"/>
         <location filename="../sidebar/sidebar.cpp" line="358"/>
         <source>For %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho %1</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="195"/>
         <source>Mount</source>
-        <translation type="unfinished"></translation>
+        <translation>Gắn</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="199"/>
         <source>Unmount</source>
-        <translation type="unfinished"></translation>
+        <translation>Tháo</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="202"/>
         <location filename="../sidebar/sidebar.cpp" line="246"/>
         <source>Couldn&apos;t unmount</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể tháo</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="203"/>
         <location filename="../sidebar/sidebar.cpp" line="247"/>
         <source>Unmounting the drive failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoạt động tháo đĩa không thành công.</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="213"/>
         <source>Eject</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhổ ra</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="216"/>
         <source>Couldn&apos;t eject</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể nhổ ra</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="217"/>
         <source>Ejecting the drive failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoạt động nhổ ổ ra bị thất bại.</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="226"/>
         <source>Lock</source>
-        <translation type="unfinished"></translation>
+        <translation>Khóa</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="230"/>
         <source>Couldn&apos;t lock</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể khóa</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="231"/>
         <source>Locking the device failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoạt động khóa đĩa thất bại.</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="257"/>
         <source>Create Disk Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Tạo ảnh đĩa</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="261"/>
         <source>Erase</source>
-        <translation type="unfinished"></translation>
+        <translation>Xóa</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="262"/>
         <source>Erase Optical Disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Xóa đĩa quang</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="297"/>
         <source>Copy In</source>
-        <translation type="unfinished"></translation>
+        <translation>Chép vào</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="300"/>
         <source>Move In</source>
-        <translation type="unfinished"></translation>
+        <translation>Chuyển vào</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="359"/>
         <source>Remove from bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Xóa khỏi thẻ đánh dấu</translation>
     </message>
 </context>
 <context>
@@ -361,12 +361,12 @@
     <message>
         <location filename="../popovers/unlockencryptedpopover.ui" line="35"/>
         <source>Unlock Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Mở khóa thiết bị</translation>
     </message>
     <message>
         <location filename="../popovers/unlockencryptedpopover.ui" line="185"/>
         <source>Enter the password for this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Điền vào mật khẩu cho thiết bị này</translation>
     </message>
 </context>
 </TS>
