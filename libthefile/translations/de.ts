89d39355e5d6228847c53ec7e2489c2a4fb31fa6
luca009 <realluca009@gmail.com>
2022-01-28 17:29:14 +0000

    Translated using Weblate (German)
    Currently translated at 94.1% (64 of 68 strings)
    
    Translation: theFile/libthefile
    Translate-URL: https://translate.vicr123.com/projects/thefile/libthefile/de/

--- libthefile/translations/de.ts
+++ libthefile/translations/de.ts
@@ -6,197 +6,199 @@
     <message>
         <location filename="../filecolumn.cpp" line="126"/>
         <source>Root</source>
-        <translation type="unfinished"></translation>
+        <translation>Root</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="127"/>
         <location filename="../filecolumn.cpp" line="216"/>
         <location filename="../filecolumn.cpp" line="471"/>
         <source>Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>Mülleimer</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="131"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Home</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="203"/>
         <location filename="../filecolumn.cpp" line="559"/>
         <source>New Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Neuer Ordner</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="203"/>
         <source>What are you naming this folder?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wie willst du diesen Ordner nennen?</translation>
     </message>
     <message numerus="yes">
         <location filename="../filecolumn.cpp" line="217"/>
         <source>Moved %n items to the trash</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n Element in den Mülleimer verschoben</numerusform>
+            <numerusform>%n Elemente in den Mülleimer verschoben</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="253"/>
         <location filename="../filecolumn.cpp" line="357"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Umbennen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="253"/>
         <source>What do you want to name this item?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wie willst du dieses Element nennen?</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="284"/>
         <location filename="../filecolumn.cpp" line="675"/>
         <source>For %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Für %1</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="287"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Öffnen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="295"/>
         <source>Open With...</source>
-        <translation type="unfinished"></translation>
+        <translation>Öffnen mit...</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="311"/>
         <source>Another app...</source>
-        <translation type="unfinished"></translation>
+        <translation>Andere App...</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="319"/>
         <source>Remove from bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Von Lesezeichen entfernen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="323"/>
         <source>Add to bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Zu Lesezeichen hinzufügen</translation>
     </message>
     <message numerus="yes">
         <location filename="../filecolumn.cpp" line="329"/>
         <source>For %n items</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Für %n Element</numerusform>
+            <numerusform>Für %n Elemente</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="333"/>
         <source>Cut</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausschneiden</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="334"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieren</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="336"/>
         <source>Restore</source>
-        <translation type="unfinished"></translation>
+        <translation>Wiederherstellen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="348"/>
         <source>Delete Permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Permanent löschen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="350"/>
         <source>Move to Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>In den Mülleimer verschieben</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="366"/>
         <source>Properties</source>
-        <translation type="unfinished"></translation>
+        <translation>Eigenschaften</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="377"/>
         <source>Burn Contents</source>
-        <translation type="unfinished"></translation>
+        <translation>Inhalte brennen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="440"/>
         <source>No items here!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Hier sind keine Elemente!</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="441"/>
         <source>This folder is empty.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dieser Ordner ist leer.</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="444"/>
         <source>Not Found</source>
-        <translation type="unfinished"></translation>
+        <translation>Nicht gefunden</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="445"/>
         <source>This folder doesn&apos;t exist.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dieser Ordner existiert nicht.</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="448"/>
         <source>Permission Denied</source>
-        <translation type="unfinished"></translation>
+        <translation>Zugriff verweigert</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="449"/>
         <source>Looks like you don&apos;t have permission to view this folder.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sieht so aus, als hättest keine Rechte auf diesen Ordner zuzugreifen.</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="452"/>
         <source>Can&apos;t view this folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Kann diesen Ordner nicht anzeigen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="453"/>
         <source>We can&apos;t show you the contents of this folder.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wir können dir den Inhalt dieses Ordner nicht anzeigen.</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="472"/>
         <source>Empty Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>Mülleimer leeren</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="558"/>
         <location filename="../filecolumn.cpp" line="685"/>
         <source>For this folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Für diesen Ordner</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="560"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>Einfügen</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="676"/>
         <source>Copy In</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieren in</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="679"/>
         <source>Move In</source>
-        <translation type="unfinished"></translation>
+        <translation>Bewegen in</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="686"/>
         <source>Copy Here</source>
-        <translation type="unfinished"></translation>
+        <translation>Hierher kopieren</translation>
     </message>
     <message>
         <location filename="../filecolumn.cpp" line="689"/>
         <source>Move Here</source>
-        <translation type="unfinished"></translation>
+        <translation>Hierher verschieben</translation>
     </message>
 </context>
 <context>
@@ -204,8 +206,9 @@
     <message numerus="yes">
         <location filename="../filecolumnfloater.cpp" line="51"/>
         <source>%n items</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n Element</numerusform>
+            <numerusform>%n Elemente</numerusform>
         </translation>
     </message>
 </context>
@@ -214,126 +217,126 @@
     <message>
         <location filename="../sidebar/sidebar.ui" line="74"/>
         <source>PLACES</source>
-        <translation type="unfinished"></translation>
+        <translation>ORTE</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.ui" line="103"/>
         <source>DRIVES</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GERÄTE</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.ui" line="135"/>
         <source>BOOKMARKS</source>
-        <translation type="unfinished"></translation>
+        <translation>LESEZEICHEN</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="66"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Home</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="67"/>
         <source>Documents</source>
-        <translation type="unfinished"></translation>
+        <translation>Dokumente</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="68"/>
         <source>Downloads</source>
-        <translation type="unfinished"></translation>
+        <translation>Downloads</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="69"/>
         <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Musik</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="70"/>
         <source>Pictures</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilder</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="71"/>
         <source>Videos</source>
-        <translation type="unfinished"></translation>
+        <translation>Videos</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="72"/>
         <source>Root</source>
-        <translation type="unfinished"></translation>
+        <translation>Root</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="73"/>
         <source>Trash</source>
-        <translation type="unfinished"></translation>
+        <translation>Mülleimer</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="192"/>
         <location filename="../sidebar/sidebar.cpp" line="296"/>
         <location filename="../sidebar/sidebar.cpp" line="358"/>
         <source>For %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Für %1</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="195"/>
         <source>Mount</source>
-        <translation type="unfinished"></translation>
+        <translation>Mount</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="199"/>
         <source>Unmount</source>
-        <translation type="unfinished"></translation>
+        <translation>Unmount</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="202"/>
         <location filename="../sidebar/sidebar.cpp" line="246"/>
         <source>Couldn&apos;t unmount</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte nicht unmounten</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="203"/>
         <location filename="../sidebar/sidebar.cpp" line="247"/>
         <source>Unmounting the drive failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Unmounten des Geräts scheiterte.</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="213"/>
         <source>Eject</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswerfen</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="216"/>
         <source>Couldn&apos;t eject</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte nicht aufwerfen</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="217"/>
         <source>Ejecting the drive failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswerfen des Geräts scheiterte.</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="226"/>
         <source>Lock</source>
-        <translation type="unfinished"></translation>
+        <translation>Sperren</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="230"/>
         <source>Couldn&apos;t lock</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte nicht sperren</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="231"/>
         <source>Locking the device failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sperren des Geräts scheiterte.</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="257"/>
         <source>Create Disk Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Disk-Image erstellen</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="261"/>
         <source>Erase</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="262"/>
@@ -343,17 +346,17 @@
     <message>
         <location filename="../sidebar/sidebar.cpp" line="297"/>
         <source>Copy In</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieren in</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="300"/>
         <source>Move In</source>
-        <translation type="unfinished"></translation>
+        <translation>Bewegen in</translation>
     </message>
     <message>
         <location filename="../sidebar/sidebar.cpp" line="359"/>
         <source>Remove from bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Von Lesezeichen entfernen</translation>
     </message>
 </context>
 <context>
@@ -361,12 +364,12 @@
     <message>
         <location filename="../popovers/unlockencryptedpopover.ui" line="35"/>
         <source>Unlock Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Gerät entsperren</translation>
     </message>
     <message>
         <location filename="../popovers/unlockencryptedpopover.ui" line="185"/>
         <source>Enter the password for this device</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebe das Password für dieses Gerät ein</translation>
     </message>
 </context>
 </TS>
